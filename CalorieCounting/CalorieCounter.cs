55182fb2b5891c86a49bbc1d180b9f105fdfa566
daviscruz <dcruz@bitworks.com.sv>
2022-12-03 03:47:54 -0600

    AdventOfCode day 1 solved

---
+++ CalorieCounting/CalorieCounter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieCounting
+{
+    public class CalorieCounter
+    {
+        public int GetTheMostCalories()
+        {
+            string[] calories = File.ReadAllLines(@"input.txt");
+
+            List<int> elfCalories = new List<int>();
+
+            int currentElfCalories = 0;
+            foreach (var calory in calories)
+            {
+                if (string.IsNullOrEmpty(calory))
+                {
+                    elfCalories.Add(currentElfCalories);
+                    currentElfCalories = 0;
+                    continue;
+                }
+
+                currentElfCalories += int.Parse(calory);
+            }
+
+            return elfCalories.Max();
+        }
+        public int[] GetTopThreeCalories()
+        {
+            string[] calories = File.ReadAllLines(@"input.txt");
+
+            List<int> elfCalories = new List<int>();
+
+            int currentElfCalories = 0;
+            foreach (var calory in calories)
+            {
+                if (string.IsNullOrEmpty(calory))
+                {
+                    elfCalories.Add(currentElfCalories);
+                    currentElfCalories = 0;
+                    continue;
+                }
+
+                currentElfCalories += int.Parse(calory);
+            }
+
+            return elfCalories.OrderByDescending(x => x)
+                              .Take(3)
+                              .ToArray();
+        }
+
+
+    }
+}
---
+++ CalorieCounting/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using CalorieCounting;
+
+CalorieCounter calorieCounting = new CalorieCounter();
+
+Console.WriteLine("Elfo que consumio mas calorias");
+
+Console.WriteLine(calorieCounting.GetTheMostCalories());
+
+Console.WriteLine("Suma del top tres de elfos que comieron mas calorias");
+
+Console.WriteLine(calorieCounting.GetTopThreeCalories().Sum());
