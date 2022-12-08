3972f27097f4e72cfec66f2f6abd473d19b014b5
daviscruz <dcruz@bitworks.com.sv>
2022-12-07 20:05:07 -0600

    Day 5 solved

---
+++ TuningTrouble/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+string[] input = await File.ReadAllLinesAsync(@"input.txt");
+
+string singleLine = string.Join("", input);
+
+int startOfPacketMarket = FindMarker(singleLine, expectedUniqueChars:4);
+int startOfMessageMarket = FindMarker(singleLine, expectedUniqueChars:14);
+
+Console.WriteLine($"first packet marker after character {startOfPacketMarket}");
+Console.WriteLine($"first message marker after character {startOfMessageMarket}");
+
+
+static int FindMarker(string input, int expectedUniqueChars)
+{
+    List<char> chars = new List<char>();
+
+    int scapeCount = 0;
+
+    foreach (var item in input.ToList())
+    {
+        scapeCount++;
+        chars.Add(item);
+
+        if (chars.Count() != expectedUniqueChars)
+            continue;
+
+        if (IsMarker(chars, expectedUniqueChars))
+            break;
+
+        chars.RemoveAt(0);
+    }
+
+    return scapeCount;
+}
+
+static bool IsMarker(List<char> chars, int expectedUniqueChars)
+{
+    return chars.Distinct().Count() == expectedUniqueChars;
+}
